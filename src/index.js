90d03e470d24fd09ab9c05aa0ec6eef612c13410
Darya Sivitskaya <dara.sivitskaya@gmail.com>
2024-10-27 23:02:33 +0100

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let morseCode = '';
+
+    for (let i = 0; i < expr.length; i += 2) {
+        let symbol = expr.slice(i, i + 2);
+
+        if (i % 10 === 0 && i !== 0) {
+            if (morseCode) {
+                result += MORSE_TABLE[morseCode];
+                morseCode = '';
+            }
+            if (symbol === '**') {
+                result += ' ';
+                i += 8;
+                continue;
+            }
+        }
+        if (symbol === '10') morseCode += '.';
+        else if (symbol === '11') morseCode += '-';
+    }
+    if (morseCode) {
+        result += MORSE_TABLE[morseCode];
+    }
+    return result;
 }
 
 module.exports = {
